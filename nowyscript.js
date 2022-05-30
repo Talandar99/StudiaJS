4f0e7bcb55cd3de559fa1a6b8e316395d8e9eeed
Talandar <32677600+Talandar99@users.noreply.github.com>
2022-05-30 16:35:13 +0200

    save

--- nowyscript.js
+++ nowyscript.js
@@ -1,10 +1,21 @@
-		function loadXMLDoc() {
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function() {
-              if (this.readyState == 4 && this.status == 200) {
-                display(this);
-              }
-            };
-            xmlhttp.open("GET", "baza.xml", true);
-            xmlhttp.send();
-          }
+function loadXMLDoc() {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+        	if (this.readyState == 4 && this.status == 200) {
+         	display(this);
+		}
+	};
+	xmlhttp.open("GET", "przychodnia.xml", true);
+	xmlhttp.send();
+}
+
+function display(xml) {
+	var xmlDoc = xml.responseXML;
+	var ludzie = Array.from( xmlDoc.getElementsByTagName("osoba"));
+	ludzie = ludzie.filter( p => (p.querySelector("miasto").innerHTML == "Bydgoszcz"));
+	let table = document.querySelector("table");
+	displayTable(table, ludzie);
+}	
+
+
+loadXMLDoc();
