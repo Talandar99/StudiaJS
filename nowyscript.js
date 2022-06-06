ccd3cc55147d4ac5c069fdd72baf3bc0333da164
Talandar99 <grzegorzc66@gmail.com>
2022-06-06 17:30:52 +0200

    rekurencja

--- nowyscript.js
+++ nowyscript.js
@@ -1,8 +1,8 @@
 function loadXMLDoc() {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-        	if (this.readyState == 4 && this.status == 200) {
-         	display(this);
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			display(this);
 		}
 	};
 	xmlhttp.open("GET", "przychodnia.xml", true);
@@ -11,16 +11,17 @@ function loadXMLDoc() {
 
 function display(xml) {
 	var xmlDoc = xml.responseXML;
-	var gabinety = Array.from( xmlDoc.getElementsByTagName("gabinet"));
-	for(var i=0; i<gabinety.length;i++){
-		gabinety[i].forEach(element => {
-			console.log(element);
+	var gabinety = Array.from(xmlDoc.getElementsByTagName("gabinet"));
+	for (var i = 0; i < gabinety.length; i++) {
+		gabinety[i].childNodes.forEach(child => {
+			if (child.childElementCount != null) {
+				if (child.childElementCount == 0) {
+					console.log(child.innerHTML);
+				} else {
+					console.log(child);
+				}
+			}
 		});
-		
 	}
-}	
+}
 loadXMLDoc();
-// var warzywa = ["Kapusta", "Rzepa", "Rzodkiew", "Marchew"];
-// warzywa.forEach(element => {
-//     console.log(element);
-// });
