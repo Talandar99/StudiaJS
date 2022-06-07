18592d56a8a9ea63975ebd6d240f30e47950291e
Talandar99 <grzegorzc66@gmail.com>
2022-06-07 10:24:26 +0200

    recursive solution

--- nowyscript.js
+++ nowyscript.js
@@ -1,27 +1,27 @@
 function loadXMLDoc() {
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function () {
-		if (this.readyState == 4 && this.status == 200) {
+		if (this.readyState == 4 && this.status == 200)
 			display(this);
-		}
 	};
 	xmlhttp.open("GET", "przychodnia.xml", true);
 	xmlhttp.send();
 }
 
+function dig(parent) {
+	if (parent.childElementCount != null) {
+		if (parent.childElementCount == 0) {
+			console.log("[" + parent.localName + "]			" + parent.innerHTML);
+		} else {
+			parent.childNodes.forEach(child => { dig(child) });
+		}
+	}
+}
+
 function display(xml) {
 	var xmlDoc = xml.responseXML;
-	var gabinety = Array.from(xmlDoc.getElementsByTagName("gabinet"));
-	for (var i = 0; i < gabinety.length; i++) {
-		gabinety[i].childNodes.forEach(child => {
-			if (child.childElementCount != null) {
-				if (child.childElementCount == 0) {
-					console.log(child.innerHTML);
-				} else {
-					console.log(child);
-				}
-			}
-		});
-	}
+	document.getElementById('body').appendChild(table);
+	var mainArray = Array.from(xmlDoc.getElementsByTagName("gabinet"));
+	mainArray.forEach(element => { dig(element);});
 }
 loadXMLDoc();
