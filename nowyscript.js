7a05fc6227fd1c84f6c06f61a55ca68a82895e08
Talandar99 <grzegorzc66@gmail.com>
2022-06-07 23:51:10 +0200

    coś tam działa coś tam

--- nowyscript.js
+++ nowyscript.js
@@ -1,37 +1,26 @@
-function loadXMLDoc() {
+function loadXMLDoc(nazwadokumentu) {
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function () {
-		if (this.readyState == 4 && this.status == 200)
-			display(this);
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementsByTagName('body')[0].appendChild(document.createElement('table')).setAttribute('border', '2');
+			this.responseXML.childNodes.forEach(element => { dig(element, document.getElementsByTagName('table')[0]); });
+		}
 	};
-	xmlhttp.open("GET", "przychodnia.xml", true);
+	xmlhttp.open("GET", nazwadokumentu, true);
 	xmlhttp.send();
 }
 
 function dig(parent, table) {
-
 	if (parent.childElementCount != null) {
 		if (parent.childElementCount == 0) {
+			table.insertRow();
+			document.getElementsByTagName('tr')[document.getElementsByTagName('tr').length - 1].insertCell().innerHTML = parent.localName+": ";
 			document.getElementsByTagName('tr')[document.getElementsByTagName('tr').length - 1].insertCell().innerHTML = parent.innerHTML;
+
 		} else {
 			table.insertRow().innerHTML = (parent.localName);
-			document.getElementsByTagName('tr')[document.getElementsByTagName('tr').length - 1];
 			parent.childNodes.forEach(child => { dig(child, table) });
 		}
-	} else {
-		table.insertRow();
 	}
 }
-
-function display(xml) {
-	var startingPoint = "gabinet";
-	var xmlDoc = xml.responseXML;
-	var mainArray = Array.from(xmlDoc.getElementsByTagName(startingPoint));
-	document.getElementsByTagName('body')[0].appendChild(document.createElement('table'));
-	table = document.getElementsByTagName('table')[0];
-	table.setAttribute('border', '1');
-
-	mainArray.forEach(element => { dig(element, table); });
-
-}
-loadXMLDoc();
+loadXMLDoc("przychodnia.xml");// nazwa xml'a
